c458aa7592040375c76da87f57fee034ea1e5265
unknown <jadebrosas143@gmail.com>
2026-09-23 09:19:01 +0800

    new

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://tssdappregion8.online'],
+    'allowed_origins' => ['http://localhost:5173', 'https://tssdappregion8.online', 'https://tupadscanner.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
