e9506a1bc596129d362cd95b6bbba1ae15c96cf4
unknown <jadebrosas143@gmail.com>
2026-09-23 10:55:13 +0800

    new cors

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
         'https://tupadscanner.onrender.com',
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://admirable-belekoy-35b3fd.netlify.app/login',
     ],
 
     'allowed_origins_patterns' => [],
